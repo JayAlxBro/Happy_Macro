9c38d20fe18172edcb8a57a73fd8d0395066c98e
Jayla Brown <jayla@Jaylas-iMac.local>
2022-04-17 09:49:21 -0500

    Initial commit

---
+++ app/src/main/java/com/cpst343/happymacro/MainActivity.java
@@ -0,0 +1,22 @@
+package com.cpst343.happymacro;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void onSignUpButtonClick(View view) {
+        Intent intent = new Intent(this, goal.class);
+        startActivity(intent);
+    }
+}
