c35e463490c84e7db79577bb0852c3bba0014bbc
Jayla <Jayalxbro@gmail.com>
2022-04-24 16:57:38 -0500

    added tracker pages (this included 2 extra fragement java pages) I also updated colors.xml to have the figma colors. I updated the colors in the themes.xml. I deleted the themes.xml(night)

---
+++ app/src/main/java/com/cpst343/happymacro/Tracker.java
@@ -0,0 +1,51 @@
+package com.cpst343.happymacro;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.cpst343.happymacro.databinding.ActivityTrackerBinding;
+
+public class Tracker extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityTrackerBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityTrackerBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_tracker);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_tracker);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
