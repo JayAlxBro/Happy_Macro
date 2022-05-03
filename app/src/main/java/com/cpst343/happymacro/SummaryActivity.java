04bc6e25284a86c1d2e35dd350b6be5ae9bde281
Aisha Rana <arana1@luc.edu>
2022-05-03 08:19:39 -0500

    Implements Nav Bar

--- app/src/main/java/com/cpst343/happymacro/SummaryActivity.java
+++ app/src/main/java/com/cpst343/happymacro/SummaryActivity.java
@@ -1,18 +1,45 @@
 package com.cpst343.happymacro;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.SeekBar;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class SummaryActivity extends AppCompatActivity {
+
+    NavigationBarView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+                bottomNavigationView.setSelectedItemId(R.id.home);
+                bottomNavigationView.setOnItemSelectedListener(item -> {
+
+                    switch (item.getItemId()) {
+                        case R.id.edit:
+                            startActivity(new Intent(getApplicationContext(), Tracker.class));
+                            overridePendingTransition(0, 0);
+                            return true;
+                        case R.id.home:
+                            return true;
+                        case R.id.check:
+                            startActivity(new Intent(getApplicationContext(), BodyGaols.class));
+                            overridePendingTransition(0, 0);
+                            return true;
+
+                    }
+                    return false;
+                });
 
 
         SeekBar seekBar = findViewById(R.id.seek_bar);
